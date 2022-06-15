4e1c07bc69e6f50cd1e93966c04df11ebe48795c
houssam-dev <developpeur.pro.ma@gmail.com>
2022-06-15 12:09:21 +0100

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import { useEffect, useRef, useState } from 'react';
+import './App.scss';
+
+
+function App() {
+
+	return (
+		<div className="">
+			<h1>Hello world!</h1>
+		</div>
+	)
+}
+
+export default App;
