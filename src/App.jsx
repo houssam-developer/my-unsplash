10d9bf56625e8cfc3ec95063a7eeb3060921a2ad
houssam-dev <developpeur.pro.ma@gmail.com>
2022-06-16 06:18:45 +0100

    style: add skeleton masonry layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@
 import { useEffect, useRef, useState } from 'react';
 import './App.scss';
 import { MdPerson, MdSearch } from "react-icons/md";
+import path from 'path';
 
 
 function App() {
 
 	return (
-		<div className="p-4 w-full">
+		<div className="p-3 w-full flex flex-col gap-14">
 			<header className="flex flex-wrap gap-4 md:gap-8 lg:gap-10 xl:gap-14" >
 				{/* User */}
 				<div className="p-1 flex items-center gap-2 self-start" >
@@ -25,8 +26,23 @@ function App() {
 				</div>
 
 				{/* Button */}
-				<button className='py-4 px-5 text-xs sm:text-base ml-auto rounded-xl text-white font-bold bg-[#3DB46D] '>Add a photo</button>
+				<button className='p-4 px-8 text-xs sm:text-base ml-auto rounded-xl text-white font-bold bg-[#3DB46D] '>Add a photo</button>
 			</header>
+
+			<main>
+				<ul className='container-masonry'>
+					{[...Array(7).keys()].map((it, idx) =>
+						idx === 1 || idx === 6 ?
+							<li className='max-w-full card-tall'>
+								<img className=' max-w-full  rounded-2xl' src={`/stock-images/mystock-image-${it}.jpg`} alt="" srcset="" />
+							</li>
+							:
+							<li className='max-w-full max-h-[280px]'>
+								<img className=' rounded-2xl' src={`/stock-images/mystock-image-${it}.jpg`} alt="" srcset="" />
+							</li>
+					)}
+				</ul>
+			</main>
 		</div>
 	)
 }
