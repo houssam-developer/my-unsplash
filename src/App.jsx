23bdc160be191151cbbb87719560ba2f12248a31
houssam-dev <developpeur.pro.ma@gmail.com>
2022-06-17 12:04:32 +0100

    refactor: add skeleton service

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ function App() {
 		<div className="">
 			<ModalAddNewPhoto key={showModalAddNewPhotoFlag} showModal={showModalAddNewPhotoFlag} fnCloseModal={closeModal} />
 			<ModalDeletePhoto key={showModalDeletePhotoFlag} showModal={showModalDeletePhotoFlag} fnCloseModal={closeModal} />
-			<div className="px-2 py-4 w-full flex flex-col gap-14 full-page-filter">
+			<div className="px-2 py-2 sm:py-8 w-full flex flex-col gap-14 full-page-filter">
 				<header className="flex flex-wrap gap-4 md:gap-8 lg:gap-10 xl:gap-14 ">
 					{/* User */}
 					<div className="p-1 flex items-center gap-2 self-start" >
---
+++ src/services/UnsplashService.js
@@ -0,0 +1,14 @@
+export const unsplashService = (function () {
+	const API_URL_BASE = '';
+
+	function findAll() { }
+	function findByKeyword(keyword) { }
+
+	function addNewPhoto() { }
+	function deletePhoto(id) { }
+
+
+	return {
+
+	}
+})();
